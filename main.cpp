1796cd18b38b603a3a902245f7ee90bc373e1a9d
maksymdidukh <maxym.didukh@logivations.com>
2023-12-23 16:41:04 +0100

    change main to show usage of all operations

--- main.cpp
+++ main.cpp
@@ -2,17 +2,40 @@
 #include "GaloisField.hpp"
 
 int main() {
-    GaloisField first("4fc1c8f723d908a8459557b73b1d9335db15946ae1cd3c638b407ea812c");
-    GaloisField second("6c6e49eca4f2b4b5684cc678e5b1449500a6ae03b4e732eaacdec175ccd");
+    GaloisField a("4fc1c8f723d908a8459557b73b1d9335db15946ae1cd3c638b407ea812c");
+    GaloisField b("6c6e49eca4f2b4b5684cc678e5b1449500a6ae03b4e732eaacdec175ccd");
+    
+    std::cout << "\na: ";
+    a.printBits();
 
-    first.printBits();
-    second.printBits();
+    std::cout << "\nb: ";
+    b.printBits();
 
-    auto sum = first + second;
-    sum.printBits();
+    std::cout << "\n+: ";
+    auto res = a + b;
+    res.printBits();
 
-    sum = first.toPowerOf(second);
-    sum.printBits();
+    std::cout << "\n*: ";
+    res = a * b;
+    res.printBits();
+
+    std::cout << "\nto square: ";
+    res = a.toSquare();
+    res.printBits();
+
+    std::cout << "\nto power of: ";
+    res = a.toPowerOf(b);
+    res.printBits();
+
+    std::cout << "\ninverse: ";
+    res = a.inverse();
+    res.printBits();
+
+    int trace = a.trace();
+    std::cout << "\nTrace a: " << trace << std:: endl;
+    
+    trace = b.trace();
+    std::cout << "\nTrace b: " << trace << std:: endl;
 
     return 0;
 }
